3e8a1ae4cd70a5cd2bb23348b588642a9c54d8f5
meghabista <89595947+meghabista@users.noreply.github.com>
2023-05-03 19:31:10 -0500

    Create models.py

---
+++ Models/models.py
@@ -0,0 +1,151 @@
+##Libraries##
+
+
+#Model
+from keras.models import Model
+from keras.layers import Layer
+from keras.layers import Conv2D
+from keras.layers import Dropout
+from keras.layers import UpSampling2D
+from keras.layers import concatenate
+from keras.layers import Add
+from keras.layers import Multiply
+from keras.layers import Input
+from keras.layers import MaxPool2D
+from keras.layers import BatchNormalization
+
+# Callbacks 
+from keras.callbacks import Callback
+from keras.callbacks import EarlyStopping
+from keras.callbacks import ModelCheckpoint
+from tf_explain.core.grad_cam import GradCAM
+
+
+##Encoder##
+class EncoderBlock(Layer):
+
+    def __init__(self, filters, rate, pooling=True, **kwargs):
+        super(EncoderBlock, self).__init__(**kwargs)
+
+        self.filters = filters
+        self.rate = rate
+        self.pooling = pooling
+
+        self.c1 = Conv2D(filters, kernel_size=3, strides=1, padding='same', activation='relu', kernel_initializer='he_normal')
+        self.drop = Dropout(rate)
+        self.c2 = Conv2D(filters, kernel_size=3, strides=1, padding='same', activation='relu', kernel_initializer='he_normal')
+        self.pool = MaxPool2D()
+
+    def call(self, X):
+        x = self.c1(X)
+        x = self.drop(x)
+        x = self.c2(x)
+        if self.pooling:
+            y = self.pool(x)
+            return y, x
+        else:
+            return x
+
+    def get_config(self):
+        base_config = super().get_config()
+        return {
+            **base_config,
+            "filters":self.filters,
+            'rate':self.rate,
+            'pooling':self.pooling
+        }
+   
+## Decoder##
+ class DecoderBlock(Layer):
+
+  def __init__(self, filters, rate, **kwargs):
+        super(DecoderBlock, self).__init__(**kwargs)
+
+        self.filters = filters
+        self.rate = rate
+
+        self.up = UpSampling2D()
+        self.net = EncoderBlock(filters, rate, pooling=False)
+
+  def call(self, X):
+        X, skip_X = X
+        x = self.up(X)
+        c_ = concatenate([x, skip_X])
+        x = self.net(c_)
+        return x
+
+  def get_config(self):
+        base_config = super().get_config()
+        return {
+            **base_config,
+            "filters":self.filters,
+            'rate':self.rate,
+        }
+      
+  ## Attention Gate ##
+  class AttentionGate(Layer):
+
+   def __init__(self, filters, bn, **kwargs):
+        super(AttentionGate, self).__init__(**kwargs)
+
+        self.filters = filters
+        self.bn = bn
+
+        self.normal = Conv2D(filters, kernel_size=3, padding='same', activation='relu', kernel_initializer='he_normal')
+        self.down = Conv2D(filters, kernel_size=3, strides=2, padding='same', activation='relu', kernel_initializer='he_normal')
+        self.learn = Conv2D(1, kernel_size=1, padding='same', activation='sigmoid')
+        self.resample = UpSampling2D()
+        self.BN = BatchNormalization()
+
+  def call(self, X):
+        X, skip_X = X
+
+        x = self.normal(X)
+        skip = self.down(skip_X)
+        x = Add()([x, skip])
+        x = self.learn(x)
+        x = self.resample(x)
+        f = Multiply()([x, skip_X])
+        if self.bn:
+            return self.BN(f)
+        else:
+            return f
+        # return f
+
+    def get_config(self):
+        base_config = super().get_config()
+        return {
+            **base_config,
+            "filters":self.filters,
+            "bn":self.bn
+        }
+## Callback##
+class ShowProgress(Callback):
+    def on_epoch_end(self, epochs, logs=None):
+        id = np.random.randint(200)
+        exp = GradCAM()
+        image = images[id]
+        mask = masks[id]
+        pred_mask = self.model.predict(image[np.newaxis,...])
+        cam = exp.explain(
+            validation_data=(image[np.newaxis,...], mask),
+            class_index=1,
+            layer_name='Attention4',
+            model=self.model
+        )
+
+        plt.figure(figsize=(10,5))
+
+        plt.subplot(1,3,1)
+        plt.title("Original Mask")
+        show_mask(image, mask, cmap='copper')
+
+        plt.subplot(1,3,2)
+        plt.title("Predicted Mask")
+        show_mask(image, pred_mask, cmap='copper')
+
+        plt.subplot(1,3,3)
+        show_image(cam,title="GradCAM")
+        
+        plt.tight_layout()
+        plt.show()
